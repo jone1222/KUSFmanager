349802813da0a1874121de9162ac5c557d7fed13
jone1222 <jone1222@hanmail.net>
2017-11-20 22:08:53 +0900

    - Connected Database : db/MySQLiteDB - Database.java - Database.open : Initialize DB Connection  / Database readMeta(query) : get result of query - Add member variable in Room ( rName : RoomName, capacity : maximum available users )

--- src/mainInterface/MainProgram.java
+++ src/mainInterface/MainProgram.java
@@ -1,10 +1,16 @@
 package mainInterface;
 
+import java.sql.SQLException;
+
+import unitDatabase.Database;
+
 public class MainProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-
+		Database DB = new Database();
+		DB.open();
+		DB.readMeta(" ");
 	}
 
 }
--- src/unitClass/Room.java
+++ src/unitClass/Room.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Room {
 	
 	private int rid;
+	private String rName;
+	private int capacity;
 	private ArrayList<Reservation> reservations;
 	private ArrayList<Item> items;
 	
@@ -23,5 +25,13 @@ public class Room {
 	public ArrayList<Item> getItems() {
 		return items;
 	}
-	
+
+	public String getrName() {
+		return rName;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
 }
