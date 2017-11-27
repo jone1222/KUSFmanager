0e81aa74c8f6031663b5a3a635c8ed626a91eef9
YeEun <s_yeaheun@naver.com>
2017-11-27 20:29:37 +0900

    gg

--- src/mainInterface/MainProgram.java
+++ src/mainInterface/MainProgram.java
@@ -1,88 +1,115 @@
 package mainInterface;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;  
 //import unitDatabase.Database; 
 
 public class MainProgram extends JFrame implements ActionListener {
+	int screenHeight;
+	int screenWidth;
+	
 	private JButton btn1,btn2,btn3,btn4,btn5,btn6,btn7;
 	private JFrame frm;
 	private JTabbedPane tabbedPane;
 	private JPanel panel1;
 	private JPanel panel2;
-	private JPanel panel3;
-	private JLabel label1;
-	private JLabel label2;
 	CardLayout card;
 	JPanel card1, card2, card3,card4;
 	
-	public MainProgram() {
+	public MainProgram() throws IOException {
 		frm = new JFrame();
+		Container c = frm.getContentPane();
+		
 		tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
-//		panel1 = new JPanel(); // 이거 대신 카드 레이아웃을 사용
+		//panel1 = new JPanel(); // 이거 대신 카드 레이아웃을 사용
 		panel2 = new JPanel();
 		
-		label1 = new JLabel("예약하기");
-		label2 = new JLabel("예약확인");
-		btn1 = new JButton("다음화면"); 		
-		btn1.addActionListener(this);
-		btn2 = new JButton("전화면"); 		
+		btn1 = new JButton("다음"); 
+		btn1.addActionListener(this); 
+		btn1.setBounds(1000, 650, 100, 20);
+		
+		btn2 = new JButton("이전"); 		
 		btn2.addActionListener(this);
-		btn3 = new JButton("다음화면"); 		
+		btn2.setBounds(900, 650, 100, 20);
+		
+		btn3 = new JButton("다음"); 	
 		btn3.addActionListener(this);
-		btn4 = new JButton("전화면"); 		
+		btn3.setBounds(1000, 650, 100, 20);
+		
+		btn4 = new JButton("전화면3"); 		
 		btn4.addActionListener(this);
-		btn5 = new JButton("다음화면"); 		
+		
+		btn5 = new JButton("다음화면3"); 		
 		btn5.addActionListener(this);
-		btn6 = new JButton("전화면"); 		
+		
+		btn6 = new JButton("전화면4"); 		
 		btn6.addActionListener(this);
-		btn7 = new JButton("다음화면"); 		
+		
+		btn7 = new JButton("다음화면4"); 		
 		btn7.addActionListener(this);
 		
-		
 		card = new CardLayout();
-		
 		panel1 = new JPanel(card);
-		card1 = new JPanel();
+		
+		card1 = new JPanel(); 
+		card1.setLayout(null);
+		
+		BufferedImage img1 = ImageIO.read(new File("img\\floor1.jpg"));
+		JLabel floor1 = new JLabel(new ImageIcon(img1));
+		floor1.setSize(600, 600); floor1.setLocation(0, 0);
+		BufferedImage img2 = ImageIO.read(new File("img\\floor2.jpg"));
+		JLabel floor2 = new JLabel(new ImageIcon(img2));
+		floor2.setSize(600, 600); floor2.setLocation(650, 0);
+		card1.add(floor1);
+		card1.add(floor2);
+		
 		card2 = new JPanel();
+		card2.setLayout(null);
+		
+		
 		card3 = new JPanel();
+		card3.setLayout(null);
 		card4 = new JPanel();
+		card4.setLayout(null);
+		
+		card1.add(btn1);// 다음으로 눌러서 예약화면 넘어가기
+		card2.add(btn2); // 두번쨰  화면에서 버튼 되돌아가기
+		card2.add(btn3);// 두번쨰 화면에서 버튼 다음화면
 		
 		panel1.add("1",card1);
 		panel1.add("2",card2);
 		panel1.add("3",card3);
 		panel1.add("4",card4);
-//		panel1.add(label1);
-		panel2.add(label2);
-//		panel1.add(btn1);
-		card1.add(btn1);// 다음으로 눌러서 예약화면 넘어가기
-		
-		card2.add(btn2); // 두번쨰  화면에서 버튼 되돌아가기
-		card2.add(btn3);// 두번쨰 화면에서 버튼 다음화면
-		
+
 		card3.add(btn4);
 		card3.add(btn5);
-		
 		
 		tabbedPane.add("reserve", panel1);
 		tabbedPane.add("checkReserve", panel2);
 		
-		frm.add(tabbedPane);
-		frm.setSize(500, 500);
+		c.add(tabbedPane);
 		frm.setTitle("예약");
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm.setVisible(true);
 		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize(); // 스크린의 폭과 높이 정보를 가짐
+		screenHeight = screenSize.height;
+		screenWidth = screenSize.width;
+		frm.setSize(screenWidth, screenHeight); // 사이즈 지정
+		frm.setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==btn1) {
 			card.show(panel1, "2");
-			//panel3 = new JPanel(); 여기 공부중입니당
 		}
 		else if(e.getSource()==btn2) {
 			card.show(panel1, "1");
@@ -98,9 +125,8 @@ public class MainProgram extends JFrame implements ActionListener {
 		}
 		
 	}
-
-	public static void main(String[] args) throws SQLException {
-
+	
+	public static void main(String[] args) throws SQLException, IOException {
 		new MainProgram();
 	}
 
--- src/unitDatabase/Database.java
+++ src/unitDatabase/Database.java
@@ -1,88 +1,88 @@
-package unitDatabase;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.sqlite.SQLiteConfig;
-import org.sqlite.SQLiteOpenMode;
-
-public class Database {
-	public static String workingDir = System.getProperty("user.dir");
-	public final static String DATABASE = "\\db\\MySQLiteDB";
-	
-	private String dbFileName;
-	private boolean isOpened = false;
-	private Connection connection;
-	
-	static {
-		try { 
-			Class.forName("org.sqlite.JDBC"); 
-		} catch(Exception e) {
-			e.printStackTrace(); 
-		} 
-	}
-	public Database() {
-		this.dbFileName = "jdbc:sqlite:"+workingDir+DATABASE;
-		System.out.println(workingDir+DATABASE);
-	}
-	public Database(String databaseFileName) { 
-		this.dbFileName = databaseFileName;
-	}
-
-	public boolean open() {
-		try {
-			// 읽기 전용 
-			SQLiteConfig config = new SQLiteConfig(); 
-			config.setOpenMode(SQLiteOpenMode.READWRITE);
-			//this.connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\JSK\\MySQLiteDB\\", config.toProperties());
-			this.connection = DriverManager.getConnection(dbFileName, config.toProperties());
-		} catch(SQLException e) { 
-			e.printStackTrace(); return false; 
-		} 
-		isOpened = true; 
-		return true; 
-	} 
-	public boolean close() { 
-		if(this.isOpened == false) { 
-			return true; 
-		} 
-		try { 
-			this.connection.close(); 
-		} catch(SQLException e) { 
-			e.printStackTrace(); 
-			return false; 
-		} 
-		return true; 
-	}
-	public boolean readMeta(String query) throws SQLException {
-		if(this.isOpened == false) {
-			return false; 
-		} 
-		boolean result = false;  
-//		PreparedStatement prep = this.connection.prepareStatement(query); 
-//		String query = "SELECT * FROM media WHERE FilePath=? AND CheckSum=?;";
-//		prep.setString(1, filePath); 
-//		prep.setString(2, hashCode); 
-//		ResultSet row = prep.executeQuery(); 
-//		if(row.next()) { 
-//			row.getString(1);	// index 로 가져오기 
-//			row.getString("FileSize");	// field 이름으로 가져오기 
-//			result = true; 
+//package unitDatabase;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//import org.sqlite.SQLiteConfig;
+//import org.sqlite.SQLiteOpenMode;
+//
+//public class Database {
+//	public static String workingDir = System.getProperty("user.dir");
+//	public final static String DATABASE = "\\db\\MySQLiteDB";
+//	
+//	private String dbFileName;
+//	private boolean isOpened = false;
+//	private Connection connection;
+//	
+//	static {
+//		try { 
+//			Class.forName("org.sqlite.JDBC"); 
+//		} catch(Exception e) {
+//			e.printStackTrace(); 
+//		} 
+//	}
+//	public Database() {
+//		this.dbFileName = "jdbc:sqlite:"+workingDir+DATABASE;
+//		System.out.println(workingDir+DATABASE);
+//	}
+//	public Database(String databaseFileName) { 
+//		this.dbFileName = databaseFileName;
+//	}
+//
+//	public boolean open() {
+//		try {
+//			// 읽기 전용 
+//			SQLiteConfig config = new SQLiteConfig(); 
+//			config.setOpenMode(SQLiteOpenMode.READWRITE);
+//			//this.connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\JSK\\MySQLiteDB\\", config.toProperties());
+//			this.connection = DriverManager.getConnection(dbFileName, config.toProperties());
+//		} catch(SQLException e) { 
+//			e.printStackTrace(); return false; 
+//		} 
+//		isOpened = true; 
+//		return true; 
+//	} 
+//	public boolean close() { 
+//		if(this.isOpened == false) { 
+//			return true; 
+//		} 
+//		try { 
+//			this.connection.close(); 
+//		} catch(SQLException e) { 
+//			e.printStackTrace(); 
+//			return false; 
+//		} 
+//		return true; 
+//	}
+//	public boolean readMeta(String query) throws SQLException {
+//		if(this.isOpened == false) {
+//			return false; 
+//		} 
+//		boolean result = false;  
+////		PreparedStatement prep = this.connection.prepareStatement(query); 
+////		String query = "SELECT * FROM media WHERE FilePath=? AND CheckSum=?;";
+////		prep.setString(1, filePath); 
+////		prep.setString(2, hashCode); 
+////		ResultSet row = prep.executeQuery(); 
+////		if(row.next()) { 
+////			row.getString(1);	// index 로 가져오기 
+////			row.getString("FileSize");	// field 이름으로 가져오기 
+////			result = true; 
+////		}
+//		PreparedStatement prep = this.connection.prepareStatement("SELECT name FROM sqlite_master \r\n" + 
+//				"WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%'\r\n" + 
+//				"ORDER BY 1");
+//		
+//		ResultSet row = prep.executeQuery();
+//		while(row.next()) {
+//			System.out.println(row.getString("name"));
 //		}
-		PreparedStatement prep = this.connection.prepareStatement("SELECT name FROM sqlite_master \r\n" + 
-				"WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%'\r\n" + 
-				"ORDER BY 1");
-		
-		ResultSet row = prep.executeQuery();
-		while(row.next()) {
-			System.out.println(row.getString("name"));
-		}
-		row.close(); 
-		prep.close(); 
-		return result; 
-	}
-
-}
+//		row.close(); 
+//		prep.close(); 
+//		return result; 
+//	}
+//
+//}
