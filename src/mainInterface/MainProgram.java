688a54bcd2a8bcde437c7c7b9cc018e627cd56da
jone1222 <jone1222@hanmail.net>
2017-12-01 14:41:40 +0900

    fixed Variable Names & Interface Design

--- src/mainInterface/MainProgram.java
+++ src/mainInterface/MainProgram.java
@@ -27,56 +27,43 @@ public class MainProgram extends JFrame implements ActionListener {
 	int screenHeight;
 	int screenWidth;
 
-	private JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+	private JButton nextBtn, prevBtn;
 	private JFrame frm;
 	private JTabbedPane tabbedPane;
-	private JPanel panel1, panel2;
+	private JPanel mainPanel, userPanel;
+	private JPanel reservationPanel,buttonPanel;
 	private CardLayout card;
-	private JPanel card1, card2, card3, card4;
-	JPanel calan;
+	private JPanel mapCard, roomInfoCard, timeCard, userlistCard;
+	private JPanel calan;
 	private Point pt;
 	private JLabel label1;
 	private JSlider slider;
-	private JTextField textid, textname;
 
+	
 	public MainProgram() throws IOException {
 		frm = new JFrame();
+		frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		Container c = frm.getContentPane();
 		tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
-		panel2 = new JPanel();
+		userPanel = new JPanel();
 
-		btn1 = new JButton("다음");
-		btn1.addActionListener(this);
-		btn1.setBounds(1000, 650, 100, 20);
-
-		btn2 = new JButton("이전");
-		btn2.addActionListener(this);
-		btn2.setBounds(900, 650, 100, 20);
-
-		btn3 = new JButton("다음");
-		btn3.addActionListener(this);
-		btn3.setBounds(1000, 650, 100, 20);
-
-		btn4 = new JButton("이전");
-		btn4.addActionListener(this);
-		btn4.setBounds(900, 650, 100, 20);
-
-		btn5 = new JButton("다음");
-		btn5.addActionListener(this);
-		btn5.setBounds(1000, 650, 100, 20);
-
-		btn6 = new JButton("이전");
-		btn6.addActionListener(this);
-		btn6.setBounds(900, 650, 100, 20);
-
-		btn7 = new JButton("완료");
-		btn7.addActionListener(this);
-		btn7.setBounds(1000, 650, 100, 20);
+		nextBtn = new JButton("다음");
+		nextBtn.addActionListener(this);
+		
+		prevBtn = new JButton("이전");
+		prevBtn.addActionListener(this);
 
 		card = new CardLayout();
-		panel1 = new JPanel(card);
-		card1 = new JPanel();
-		card1.setLayout(null);
+		mainPanel = new JPanel(new BorderLayout());
+		
+		//divide mainPanel into two
+		reservationPanel = new JPanel(card);
+		buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		buttonPanel.add(prevBtn);
+		buttonPanel.add(nextBtn);
+		
+		mapCard = new JPanel();
+		mapCard.setLayout(null);
 
 		BufferedImage img1 = ImageIO.read(new File("img\\floor1.jpg"));
 		JLabel floor1 = new JLabel(new ImageIcon(img1));
@@ -93,46 +80,28 @@ public class MainProgram extends JFrame implements ActionListener {
 		label1 = new JLabel(pt.x + "," + pt.y);
 		label1.setSize(80, 40);
 
-		card1.add(floor1);
-		card1.add(floor2);
-		card1.add(btn1);// 다음으로 눌러서 예약화면 넘어가기
-		card1.add(label1);
-		card1.addMouseListener(new MyMAdapter());
+		mapCard.add(floor1);
+		mapCard.add(floor2);
+		mapCard.add(label1);
+		mapCard.addMouseListener(new MyMAdapter());
 
-		JPanel p = new JPanel();
-		p.setSize(50, 50);
-		p.setBackground(Color.black);
-		p.setVisible(true);
-		p.setLayout(null);
-		p.setLocation(0, 0);
+		roomInfoCard = new JPanel();
+		roomInfoCard.setLayout(null);
+		roomInfoCard.add(new roomPanel("img\\3d.png"));
 
-		card2 = new JPanel();
-		card2.setLayout(null);
-		card2.add(new roomPanel("img\\3d.png"));
-		card2.add(btn2); // 두번째 화면에서 버튼 되돌아가기
-		card2.add(btn3); // 두번쩨 화면에서 버튼 다음화면
-		card2.add(p); // 외않됌?
+		timeCard = new JPanel(new GridLayout(1, 2));
 
-		card3 = new JPanel(new GridLayout(1, 2));
-
-		// JPanel frameBottomPanel;
-		// JPanel frameSubPanelEast;
-		// JPanel frameSubPanelWest;
-
-		card3.setLayout(new GridLayout(4, 2));
+		timeCard.setLayout(new GridLayout(4, 2));
 		calendar cal = new calendar();
 		ciganpyo cig = new ciganpyo();
 
-		card3.add(cal.getcal1());
-		card3.add(cal.getcal2());
-		card3.add(cal.getcal3());
-		card3.add(cig.get_schedule());
+		timeCard.add(cal.getcal1());
+		timeCard.add(cal.getcal2());
+		timeCard.add(cal.getcal3());
+		timeCard.add(cig.get_schedule());
 
-		card3.add(btn4);
-		card3.add(btn5);
-
-		card4 = new JPanel();
-		card4.setLayout(new BorderLayout());
+		userlistCard = new JPanel();
+		userlistCard.setLayout(new BorderLayout());
 		JPanel card4_1 = new JPanel(new GridLayout(2, 2));
 		card4_1.setSize(50, 50);
 		JPanel card4_2 = new JPanel(new FlowLayout());
@@ -187,47 +156,33 @@ public class MainProgram extends JFrame implements ActionListener {
 
 		});
 
-		card4_2.add(btn6);
-		card4_2.add(btn7);
-		card4.add(card4_1, BorderLayout.CENTER);
-		card4.add(card4_2, BorderLayout.SOUTH);
+		userlistCard.add(card4_1, BorderLayout.CENTER);
+		userlistCard.add(card4_2, BorderLayout.SOUTH);
 
-		panel1.add("1", card1);
-		panel1.add("2", card2);
-		panel1.add("3", card3);
-		panel1.add("4", card4);
+		reservationPanel.add("1", mapCard);
+		reservationPanel.add("2", roomInfoCard);
+		reservationPanel.add("3", timeCard);
+		reservationPanel.add("4", userlistCard);
 
-		tabbedPane.add("reserve", panel1);
-		tabbedPane.add("checkReserve", panel2);
+		mainPanel.add(reservationPanel,BorderLayout.CENTER);
+		mainPanel.add(buttonPanel,BorderLayout.SOUTH);
+		
+		tabbedPane.add("reserve", mainPanel);
+		tabbedPane.add("checkReserve", userPanel);
 
 		c.add(tabbedPane);
 		frm.setTitle("예약");
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Toolkit kit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = kit.getScreenSize(); // 스크린의 폭과 높이 정보를 가짐
-		screenHeight = screenSize.height;
-		screenWidth = screenSize.width;
-		frm.setSize(screenWidth, screenHeight); // 사이즈 지정
 		frm.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btn1) {
-			card.show(panel1, "2");
-		} else if (e.getSource() == btn2) {
-			card.show(panel1, "1");
-		} else if (e.getSource() == btn3) {
-			card.show(panel1, "3");
-		} else if (e.getSource() == btn4) {
-			card.show(panel1, "2");
-		} else if (e.getSource() == btn5) {
-			card.show(panel1, "4");
-		} else if (e.getSource() == btn6) {
-			card.show(panel1, "3");
-		} else if (e.getSource() == btn7) {
-			JOptionPane.showMessageDialog(panel1, "예약 완료");
+		if (e.getSource() == nextBtn) {
+			card.next(reservationPanel);
+		} else if (e.getSource() == prevBtn) {
+			card.previous(reservationPanel);
 		}
 	}
 
@@ -255,15 +210,7 @@ public class MainProgram extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) throws SQLException, ParseException, IOException {
-		// new MainProgram();
-		Database DB = new Database();
-		DB.open();
-
-		String id = "jone1222";
-		String pw = "0540354";
-
-		// 로그인 가능여부 체크
-		System.out.println(DB.checkOnLogin(id, pw));
+		new MainProgram();
 	}
 
 }
