61afece4b4a15c61dd33347776382773d8f706f6
YeEun <s_yeaheun@naver.com>
2017-12-06 01:45:59 +0900

    몇시간 사용할지 선택하고 예약버튼 클릭한 경우 예외처리 1. 예약 성공 2. 이미 예약된 시간 3. 운영 시간 초과 경우에 따라서 dialog 메세지 출력 ㅎㅎ

--- src/mainInterface/ciganpyo.java
+++ src/mainInterface/ciganpyo.java
@@ -1,6 +1,7 @@
 package mainInterface;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.*;
@@ -11,20 +12,23 @@ public class ciganpyo extends JPanel {
 	JPanel show_schedule;
 	JTable table;
 	
+	String []a = {"09:00 ~ 10:00", "10:00 ~ 11:00", "11:00 ~ 12:00", "12:00 ~ 13:00",
+			"13:00 ~ 14:00", "14:00 ~ 15:00", "15:00 ~ 16:00", "16:00 ~ 17:00"};
+	String [][]b = {
+			{"0","예약 완료","예약 완료","0","0","예약 완료","0","0","0"}};
+	
+	
+	
 	public ciganpyo(calendar cal) {
-		String []a = {"09:00 ~ 10:00", "10:00 ~ 11:00", "11:00 ~ 12:00", "12:00 ~ 13:00",
-				"13:00 ~ 14:00", "14:00 ~ 15:00", "15:00 ~ 16:00", "16:00 ~ 17:00"};
-		String [][]b = {
-				{"0","예약 완료","예약 완료","0","0","예약 완료","0","0","0"}};
-		
 		//1. 모델과 데이터를 연결
 		DefaultTableModel model = new DefaultTableModel(b,a);
 		table = new JTable(model);
+		table.setRowHeight(80);
 		
 		// 결과적으로는 Jscrollpane 추가한다
 		JScrollPane sc = new JScrollPane(table);
 		
-		for(int i = 0; i < 8; i++) {
+		for(int i = 0; i < 8; i++) { // 예약 안된 부분에 버튼 추가
 			String str = (String) model.getValueAt(0, i);
 			if(str.equals("0")) {
 				table.getColumnModel().getColumn(i).setCellRenderer(new TableCell(cal));
@@ -53,28 +57,31 @@ public class ciganpyo extends JPanel {
 		
 		public TableCell(calendar cal) {
 			
-			jb = new JButton("예약");
+			jb = new JButton("예약하기");
 			
 			jb.addActionListener(e -> { // 예약 버튼 클릭시 이벤트 처리하기 
 				int row = table.getSelectedRow();
 				int col = table.getSelectedColumn();
-				
-				System.out.print(row);
-				System.out.println(col);
-				
 				int userTime = cal.userTime;
-				System.out.println(userTime);
+				int temp = 1;
 				
-				boolean temp = true;
-				
-				for(int i = 0; i < userTime; i++) {
-					if(!table.getValueAt(row, col+i).equals("0"))// 이미 예약된 경우 예약 불가
-						temp = false;
-					System.out.println(table.getValueAt(row, col+i));
+				for(int i = 0; i < userTime; i++) { 
+					if(col+i < 8) { // 운영시간 초과된 경우
+						if(!table.getValueAt(row, col+i).equals("0")) { // 이미 예약된 경우
+							temp = 0;
+							break;
+						}
+					} else 
+						temp = 2;
 				}
 				
-				if(temp == false) {
-					dialog dia = new dialog("시간 초과", "이미 예약된 시간입니다.\n다시 입력하세요.", "OK");
+				if(temp == 0) {
+					JOptionPane.showMessageDialog(null, "이미 예약된 시간입니다.\n다시 입력하세요.", "예약 오류", JOptionPane.ERROR_MESSAGE);
+					//dialog dia = new dialog("시간 초과", "이미 예약된 시간입니다.\n다시 입력하세요.", "OK");
+				} else if(temp == 1) {
+					JOptionPane.showMessageDialog(null, "예약되었습니다.", "예약 완료", JOptionPane.ERROR_MESSAGE);
+				} else if(temp == 2) {
+					JOptionPane.showMessageDialog(null, "운영 시간 초과입니다. \n다시 입력하세요.", "예약 오류", JOptionPane.ERROR_MESSAGE);
 				}
 			});
 		}
